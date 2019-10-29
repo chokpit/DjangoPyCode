80cd95b9d644451db01bf380911df867a41d36ff
afikmirc <afikmirc8@gmail.com>
2019-10-29 05:41:17 +0200

    new allow host

--- buttonpython/buttonpython/settings.py
+++ buttonpython/buttonpython/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fw@dfw(qk)(&$ah3&g2wtrc(qykd!zespsvhh@@36)yd6=a#gh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.14.67']
+ALLOWED_HOSTS = ['192.168.14.67','192.168.14.83']
 
 
 # Application definition
