61034a81a3736159902bdf9779854c9441ed790a
afikmirc <afikmirc8@gmail.com>
2019-11-05 05:46:41 +0200

    fixed shit

--- buttonpython/buttonpython/scripto.py
+++ buttonpython/buttonpython/scripto.py
@@ -1,3 +1,7 @@
+import os
+from buttonpython.settings import BASE_DIR
+
+
 def func1():
-    f = open("demofile.txt", "r") #I don't know why but the txt file must be in this path: BASE_ROOT/buttonpython/
+    f = open(os.path.join(BASE_DIR, 'templates', 'demofile.txt'), "r") #I don't know why but the txt file must be in this path: BASE_ROOT/buttonpython/
     return f.readlines()
