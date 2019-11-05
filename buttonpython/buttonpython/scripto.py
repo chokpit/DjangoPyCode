59a538fa08090efb9b2e9cd5e3d9235dba4e9ba9
afikmirc <afikmirc8@gmail.com>
2019-11-05 06:40:03 +0200

    project on minimum 1

--- buttonpython/buttonpython/scripto.py
+++ buttonpython/buttonpython/scripto.py
@@ -2,6 +2,9 @@ import os
 from buttonpython.settings import BASE_DIR
 
 
+#def func1():
+#    f = open(os.path.join(BASE_DIR, 'templates', 'demofile.txt'), "r")
+#    return f.readlines()
+
 def func1():
-    f = open(os.path.join(BASE_DIR, 'templates', 'demofile.txt'), "r") #I don't know why but the txt file must be in this path: BASE_ROOT/buttonpython/
-    return f.readlines()
+    return ("wow")
--- buttonpython/buttonpython/views.py
+++ buttonpython/buttonpython/views.py
@@ -9,8 +9,4 @@ def button(request):
     return render(request,'home.html')
 
 def output(request):
-    #data = requests.get("https://www.ynet.co.il") #good line
-    #print(data.text) #this line is not working anyways
-    #data=data.text #good line
     return render(request,'home.html',{'data':func1()})
-    #return render(request,'home.html',{'data':data})
